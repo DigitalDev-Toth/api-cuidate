e88e0be7b14fe3703ecad745372ced0fc707b7c3
Jose Miguel Lueiza Valenzuela <jose.lueiza@latam.com>
2019-06-11 15:34:44 -0400

    modificacion para construir el app y dejar una variable de entorno con el nombre del directorio donde quedo

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
-
+const dir = `dist/${pack.version}/${pack.name}`;
 const pack = require('../package.json');
-const command = `npm run prettier; node_modules/.bin/pkg -t latest-linux-x64 -o ./dist/${pack.version}/${pack.name} src/index.js`;
+const command = `npm run prettier; node_modules/.bin/pkg -t latest-linux-x64 -o ./${dir} src/index.js; cp package.json ecosystem.config.js ${dir}/; echo DIRAPP=${dir}`;
 execSync(command,{stdio: [process.stdin, process.stdout, process.stderr]});
 
