79943a47a17d4542e8094cd99782bbbd7e7901c3
Jose Miguel Lueiza Valenzuela <jose.lueiza@latam.com>
2019-06-11 15:06:39 -0400

    cambio en la forma de separar los builds

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,6 @@
 const { execSync } = require('child_process');
 
 const pack = require('../package.json');
-const name = pack.name + '-' + pack.version;
-const command = `npm run prettier; node_modules/.bin/pkg -t latest-linux-x64 -o ./dist/${name} src/index.js`;
+const command = `npm run prettier; node_modules/.bin/pkg -t latest-linux-x64 -o ./dist/${pack.version}/${pack.name} src/index.js`;
 execSync(command,{stdio: [process.stdin, process.stdout, process.stderr]});
 
